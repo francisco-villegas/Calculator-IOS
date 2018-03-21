cff73aafc6d15a236e1834790235398a9cefc4c7
Francisco Villegas <francisco.alejandre@volvocars.com>
2018-03-21 10:59:10 -0700

    Adding changes to weak reference of self and the calculator brain api is now able to add more operations from the outside. Lesson from stanford: https://www.youtube.com/watch?v=GIlsl-6TqmM&index=7&list=PLPA-ayBrweUz32NSgNZdl0_QISw-f12Ai

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,10 @@ import Foundation
 
 struct CalculatorBrain {
     
+    mutating func addUnaryOperation(named symbol: String, _ operation: @escaping (Double) -> Double) {
+        operations[symbol] = Operation.unaryOperation(operation)
+    }
+    
     private var accumulator: Double?
     
     public enum Operation { // to get the operation table from other apis like this
